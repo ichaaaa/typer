a8c7e8a87901978d9236c78ead738f0c029b7b87
ichaaaa <golinski.kamil88@gmail.com>
2021-03-10 13:21:05 +0100

    migeracje

--- database/migrations/2021_01_18_160238_create_typer_table.php
+++ database/migrations/2021_01_18_160238_create_typer_table.php
@@ -14,7 +14,7 @@ class CreateTyperTable extends Migration
     public function up()
     {
         Schema::create('typer', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             
             $table->unsignedInteger('competition_id');
             $table->unsignedBigInteger('data_provider_id');
